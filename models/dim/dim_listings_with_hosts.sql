21e8ee9ad65f325cb5ab0084dadbf334edadf329
Panyala Mythreyi <Panyala.Mythreyi@quantum-health.com>
2026-08-29 18:09:13 +0530

    initial commit

---
+++ models/dim/dim_listings_with_hosts.sql
@@ -0,0 +1,20 @@
+with listings as (
+    select * from {{ ref('dim_listings_cleansed') }}
+),
+hosts as (
+    select * from {{ ref('dim_hosts_cleansed') }}
+)
+select
+    l.listing_id,
+    l.listing_name,
+    l.room_type,
+    l.minimum_nights,
+    l.price,
+    l.host_id,
+    h.host_name,
+    h.is_superhost as host_is_superhost,
+    l.created_at as created_at,
+    greatest(l.updated_at, h.updated_at) as updated_at
+from listings l
+left join hosts h
+    on l.host_id = h.host_id
